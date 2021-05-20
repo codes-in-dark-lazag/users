6e0384793a969438048aa0df7120ce14ab789570
code-in-dark-sv <piyushohri297@gmail.com>
2021-05-20 09:18:10 +0530

    dialog + list

--- components/index.js
+++ components/index.js
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import {Dialog, DialogContent, DialogContentText,Box
+        , Table, TableBody, DialogTitle,Divider, IconButton
+          } from '@material-ui/core';
+import { CloseRounded } from "@material-ui/icons";
 
-const UsersPage = () =>{
+
+const useStyles = makeStyles({
+root: {
+  width: '100%',
+},
+rowStyle:{
+  marginTop:"20px",
+},
+
+});
+
+const UsersPage = ({users}) =>{
+    
+    const [open, setOpen] = useState(false)
+    const [curr, setCurrent] = useState({link:"", title:""})
+    const classes = useStyles();
+    const displayModal = (link, title)=>{
+      setOpen(true);
+      setCurrent({link:link, title:title})
+    }
     return (
-        <div>
-            Hi {process.env.NEXT_PUPLIC_APP_NAME}
+      <>
+        <div className="container">
+            <Table>
+                <TableBody>
+                {users.map((user) => {
+                    const {link, title,
+                        creation_date}=user;
+                    const authour_name = user.owner.display_name;
+                    return (
+                          <button onClick={()=>displayModal(link, title)}  className="stylingRow">
+                            <tr className={classes.rowStyle} >
+                              <td width="250" align="left">{authour_name}</td>
+                              <td width="450" align="center">{title}</td>
+                              <td width="350" align="right">{Date(creation_date)}</td>
+                            </tr>
+                          </button>
+                    );
+                  })}
+                </TableBody>
+            </Table>
         </div>
+        <Dialog open={open}>
+        <DialogTitle>
+          <div style={{display:"flex"}}>
+            <Box width="100%">
+             <p className="dialog-title">{curr.title}</p> 
+            </Box>
+            <IconButton className="dialog-close-icon-container" size="small"  onClick={()=>setOpen(false)}>
+                    <CloseRounded className="dialog-close-icon"/>
+            </IconButton>
+          </div> 
+          
+        </DialogTitle>
+              
+          
+          <Divider />
+          <DialogContent>
+            <DialogContentText>
+              <a className="dialog-content" href={curr.link}>{curr.link}</a>
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
+        
+      </>
     )
 }
 export default UsersPage
+
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import Head from 'next/head'
 import UsersPage from "../components";
+import fetch from  "isomorphic-fetch"
+
 const appName = "Users"
-export default function Home() {
+export default function Home({data}) {
+  const {items} = data
   return (
     <div>
       <Head>
         <title>{process.env.NEXT_PUPLIC_APP_NAME || appName} </title>
       </Head>
-      <UsersPage />
+      <UsersPage users={items}/>
     </div>
   )
+}
+export async function getServerSideProps() {
+  const res = await fetch("https://api.stackexchange.com/2.2/search/advanced?page=1&pagesize=20&o rder=desc&sort=activity&site=stackoverflow")
+  const data = await res.json()
+  return { props: { data } }
 }
